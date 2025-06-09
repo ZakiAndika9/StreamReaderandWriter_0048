2d848c966cb201272d52aa7a344d3a2cf6f8bb5c
ZakiAndika9 <m.zaki.andika9@gmail.com>
2025-06-09 11:06:23 +0700

    Input teks ke file, lalu membaca dan menampilkannya

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -18,3 +18,41 @@ int main()
 
     cout << ">= Menulis file, \'q\' untuk keluar" << endl;
 
+
+    while (true)
+    {
+        cout << "- ";
+
+        getline(cin, baris);
+
+        if (baris == "q")
+            break;
+
+        outfile << baris << endl;
+    }
+
+    outfile.close();
+
+    ifstream infile;
+
+    infile.open(namaFile + ".txt");
+
+    cout << endl
+         << ">= Membuka dan membaca file " << endl;
+
+    if (infile.is_open())
+    {
+
+        while (getline(infile, baris))
+        {
+
+            cout << baris << '\n';
+        }
+
+        infile.close();
+    }
+
+    else
+        cout << "Unable to open file";
+    return 0;
+}
